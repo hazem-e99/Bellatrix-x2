0c1fb7aee41c1832975a3f002ac46af4ec4e8627
Mostafa Anwer Hamed <121686734+mos1239487394823939839223@users.noreply.github.com>
2025-07-09 17:37:38 +0300

    Create NetSuite Consulting Page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Implementation from './components/Services/implementation';
 import Training from './components/Services/Training';
+import NetSuiteConsulting from './components/Services/NetSuiteConsulting';
 import About from './components/About/About';
 import HRSolution from './pages/HrPage/HR';
 import Payroll from './components/Payroll/Payroll';
@@ -15,6 +16,7 @@ function App() {
         <Route path="Home" element={<LandingPage />} />
         <Route path="Implementation" element={<Implementation />} />
         <Route path="Training" element={<Training />} />
+        <Route path="netsuite-consulting" element={<NetSuiteConsulting />} />
         <Route path="about" element={<About />} />
         <Route path="HRSolution" element={<HRSolution />} />
         <Route path="Payroll" element={<Payroll />} />
